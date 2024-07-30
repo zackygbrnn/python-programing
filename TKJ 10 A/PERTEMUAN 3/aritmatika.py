67ac0b66019060703b5cbea4b9901150cbc6bf5c
zackygbrnn <zackyfortuna@gmail.com>
2024-07-30 09:39:20 +0700

    Add files via upload

---
+++ TKJ 10 A/PERTEMUAN 3/aritmatika.py
@@ -0,0 +1,16 @@
+bil1 = 8
+bil2 = 2
+
+print("penjumlahan :", bil1 + bil2)
+
+print("pengurangan :", bil1 - bil2)
+
+print("perkalian :", bil1 * bil2)
+
+print("pembagian :", bil1 / bil2)
+
+print("perpangkatan :", bil1 ** bil2)
+
+print("modulus :", bil1 % bil2) 
+
+print("pembagian bulat :", bil1 // bil2)
